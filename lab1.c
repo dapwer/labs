b6ac944594a80127d8a35dbc1f5786aae4da443a
dapwer <dapwer@gmail.com>
2017-09-13 02:39:31 +0300

    Add project

--- lab1.c
+++ lab1.c
@@ -3,31 +3,31 @@
 
 int main(int argc,char **argv)
 {
-    float temp_value;
+    float inp_temp;
     if (argc >= 2)
-        temp_value = atoi(argv[1]);
+        inp_temp = atoi(argv[1]);
     if (argc == 2)
     {
-        printf("%.2f C\n",temp_value);
-        printf("%.2f F\n",(temp_value*9/5)+32);
-        printf("%.2f K\n",temp_value+273.15);
+        printf("%.2f C\n",inp_temp);
+        printf("%.2f F\n",(inp_temp*9/5)+32);
+        printf("%.2f K\n",inp_temp+273.15);
     }
     if (argc == 3)
     {
         if (argv[2][0]=='C')
         {
-            printf("%.2f F\n",(temp_value*9/5)+32);
-            printf("%.2f K\n",temp_value+273.15);
+            printf("%.2f F\n",(inp_temp*9/5)+32);
+            printf("%.2f K\n",inp_temp+273.15);
         }
         if (argv[2][0]=='F')
         {
-            printf("%.2f C\n",(temp_value-32)*5/9);
-            printf("%.2f K\n",((temp_value-32)*5/9)+273.15);
+            printf("%.2f C\n",(inp_temp-32)*5/9);
+            printf("%.2f K\n",((inp_temp-32)*5/9)+273.15);
         }
         if (argv[2][0]=='K')
         {
-            printf("%.2f C\n",temp_value-273.15);
-            printf("%.2f F\n",((temp_value-273.15)*9/5)+32);
+            printf("%.2f C\n",inp_temp-273.15);
+            printf("%.2f F\n",((inp_temp-273.15)*9/5)+32);
         }
     }
     return 0;
