5f4d0a093e3364d4dcd8c2cd5dfc335ae0f52a96
dapwer <dapwer@gmail.com>
2017-09-13 16:57:07 +0300

    Add Project

--- lab1.c
+++ lab1.c
@@ -4,31 +4,41 @@
 int main(int argc,char **argv)
 {
     double inp_temp;
+    char temp_si;
     if (argc >= 2)
         inp_temp = atoi(argv[1]);
-    if (argc == 2)
+    if (argc >= 3)
+        temp_si = argv[2][0];
+    switch (temp_si)
     {
-        printf("%.2f C\n",inp_temp);
+    case 'C':
+    {
         printf("%.2f F\n",(inp_temp*9/5)+32);
         printf("%.2f K\n",inp_temp+273.15);
     }
-    if (argc == 3)
+    break;
+    case 'F':
     {
-        if (argv[2][0]=='C')
+        printf("%.2f C\n",(inp_temp-32)*5/9);
+        printf("%.2f K\n",((inp_temp-32)*5/9)+273.15);
+    }
+    break;
+    case 'K':
+    {
+        printf("%.2f C\n",inp_temp-273.15);
+        printf("%.2f F\n",((inp_temp-273.15)*9/5)+32);
+    }
+    break;
+    default:
+    {
+        if (argc >= 2)
         {
+            printf("%.2f C\n",inp_temp);
             printf("%.2f F\n",(inp_temp*9/5)+32);
             printf("%.2f K\n",inp_temp+273.15);
         }
-        if (argv[2][0]=='F')
-        {
-            printf("%.2f C\n",(inp_temp-32)*5/9);
-            printf("%.2f K\n",((inp_temp-32)*5/9)+273.15);
-        }
-        if (argv[2][0]=='K')
-        {
-            printf("%.2f C\n",inp_temp-273.15);
-            printf("%.2f F\n",((inp_temp-273.15)*9/5)+32);
-        }
+    }
+    break;
     }
     return 0;
 }
